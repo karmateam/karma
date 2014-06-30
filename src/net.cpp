ecbdcf51a60d3c8688019448c07ae376dae7c24d
Karmateam <hirosatou071@gmail.com>
2014-06-30 12:14:15 +0100

    Update net.cpp

--- src/net.cpp
+++ src/net.cpp
@@ -1112,7 +1112,7 @@ void ThreadMapPort()
             }
         }
 
-        string strDesc = "Karmacoin " + FormatFullVersion();
+        string strDesc = "Karma " + FormatFullVersion();
 
         try {
             loop {
@@ -1192,12 +1192,12 @@ void MapPort(bool)
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strMainNetDNSSeed[][2] = {
-    {"www.karmacoin.info", "wallet01.karmacoin.info"},{"www.karmacoin.info", "wallet01.karmacoin.info"},{"www.karmacoin.info", "wallet03.karmacoin.info"},
+    {"www.karmacoin.me", "5.135.190.48"},
     {NULL, NULL}
 };
 
 static const char *strTestNetDNSSeed[][2] = {
-    {"www.karmacoin.info", "testwallet01.karmacoin.info"},{"www.karmacoin.info", "testwallet01.karmacoin.info"},{"www.karmacoin.info", "testwallet03.karmacoin.info"},
+    {"www.karmacoin.me", "5.135.190.48"},
     {NULL, NULL}
 };
 
@@ -1754,7 +1754,7 @@ bool BindListenPort(const CService &addrBind, string& strError)
     {
         int nErr = WSAGetLastError();
         if (nErr == WSAEADDRINUSE)
-            strError = strprintf(_("Unable to bind to %s on this computer. Karmacoin is probably already running."), addrBind.ToString().c_str());
+            strError = strprintf(_("Unable to bind to %s on this computer. Karma is probably already running."), addrBind.ToString().c_str());
         else
             strError = strprintf(_("Unable to bind to %s on this computer (bind returned error %d, %s)"), addrBind.ToString().c_str(), nErr, strerror(nErr));
         printf("%s\n", strError.c_str());
