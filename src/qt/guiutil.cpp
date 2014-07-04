2dee5a409161743e1fc265c711e5304bb7a773c3
Karmateam <hirosatou071@gmail.com>
2014-07-04 07:28:04 +0100

    Fix URI handling

--- src/qt/guiutil.cpp
+++ src/qt/guiutil.cpp
@@ -141,7 +141,7 @@ bool parseBitcoinURI(QString uri, SendCoinsRecipient *out)
     //    which will lower-case it (and thus invalidate the address).
     if(uri.startsWith("karma://"))
     {
-        uri.replace(0, 11, "karma:");
+        uri.replace(0, 8, "karma:");
     }
     QUrl uriInstance(uri);
     return parseBitcoinURI(uriInstance, out);
